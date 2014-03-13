3e868abd2ef2e1247d9182eeb8dc9aadde937bfc
U-RIMNET\jbethu <jbethu@2UA9490BC7.rim.net>
2014-03-13 16:06:51 -0400

    First commit

--- src/github.com/cmu440/p0/server_impl.go
+++ src/github.com/cmu440/p0/server_impl.go
@@ -2,18 +2,50 @@
 
 package p0
 
+import (
+	"fmt"
+	"net"
+	"log"
+	"strconv"
+)
+	
 type multiEchoServer struct {
 	// TODO: implement this!
+	address string
+	numClients int
 }
 
 // New creates and returns (but does not start) a new MultiEchoServer.
 func New() MultiEchoServer {
 	// TODO: implement this!
-	return nil
+	var MES MultiEchoServer
+	serv := multiEchoServer{address : "1.0.0.0"}
+	MES = &serv
+	
+	if(MES!=nil) {
+		return MES
+	} else {
+		return nil
+	}
 }
+
+func (mes *multiEchoServer) Error() string {
+	return fmt.Sprintf("Unable to start the server on address: %s",mes.address)
+} 
 
 func (mes *multiEchoServer) Start(port int) error {
 	// TODO: implement this!
+	l,err := net.Listen("tcp",mes.address + ":" + strconv.Itoa(port))
+	if(err!=nil) {
+		return mes
+	}
+	
+	for {
+		_,err := l.Accept()
+		if(err!=nil) {
+			log.Fatal(err)
+		}
+	}
 	return nil
 }
 
